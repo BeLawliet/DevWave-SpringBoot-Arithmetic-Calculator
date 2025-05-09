fa459274e2b57f2dfd2694330696172e1b4ed683
Lawliet <elcostexd995@gmail.com>
2025-05-08 22:50:22 -0500

    feat: Add JaCoCo to dependencies

--- src/test/java/arithmetic/calculator/api/ArithmeticCalculatorApplicationTests.java
+++ src/test/java/arithmetic/calculator/api/ArithmeticCalculatorApplicationTests.java
@@ -3,11 +3,10 @@ package arithmetic.calculator.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ArithmeticCalculatorApplicationTests {
-
 	@Test
 	void contextLoads() {
+		ArithmeticCalculatorApplication.main(new String[]{});
 	}
-
 }
